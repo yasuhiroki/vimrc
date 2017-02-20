43570804070f873e6d02d89d6634a7d57a268f2d
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-02-21 00:29:31 +0900

    Fix install-vim.sh

--- install-vim.sh
+++ install-vim.sh
@@ -8,7 +8,7 @@ if is_osx; then
 fi
 
 apt-get build-dep vim
-apt install gettext libncurses5-dev libacl1-dev libgpm-dev lua5.2 liblua5.2-dev luajit libluajit-5.1 libperl-dev python-dev python3-dev ruby-dev
+apt install gettext libncurses5-dev libacl1-dev libgpm-dev lua5.2 liblua5.2-dev luajit libluajit-5.1 libperl-dev python-dev python3-dev ruby-dev ncurses-dev
 
 vim_src_dir="vim-src"
 [ ! -d ${vim_src_dir} ] && git clone https://github.com/vim/vim ${vim_src_dir}
